224c948d1aaefa55daaac991443da5bd4cd916cc
Mario-78 <mariojsws1@gmail.com>
2021-10-17 14:29:36 -0300

    Minificação do arquivo sass

---
+++ css/gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const { series } = require('gulp')
+const sass = require('gulp-sass')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+
+function transformacaoCSS() {
+    return gulp.src('src/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({ "uglyComments": true }))
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('buid/css'))
+}
+
+exports.default = series(transformacaoCSS)
